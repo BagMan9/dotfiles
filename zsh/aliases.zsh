17d31e050c99d43b0f4a32c2130e78655cb105ae
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-04 12:58:23 -0400

    Update .zshrc Update zsh/aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,14 @@ alias vim='nvim'
 alias vi='nvim'
 
 # Functions -------------------------------------------------------------------
-function dotAdd {
-
+function AddSecret () {
+  local filepath="${1/#~\//}"
+  local internalPath= $(echo "$filepath" | sed 's|/\.\([^/]*\)|/dot_\1|g')
+  bw unlock
+  chezmoi add --template $1 
+  echo "{\"organizationId\":null,\"folderId\":null,\"type\":2,\"name\":\"$2\",\"notes\":\"$(cat $1 | base64)\",\"favorite\":false,\"fields\":[],\"login\":null,\"secureNote\":{\"type\":0},\"card\":null,\"identity\":null}" | bw encode | bw create item
+  chezmoi cd
+  echo "{{ (bitwarden "item" "$2").notes | b64dec }}" > $internalPath.tmpl
+  exit
+  bw lock
 }

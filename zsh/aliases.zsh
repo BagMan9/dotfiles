44fd0b682d973cfbc8a7d7b0184024ed186e5428
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-04 13:30:03 -0400

    Update zsh/aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias vi='nvim'
 # Functions -------------------------------------------------------------------
 function addsecret () {
   local filepath="${1/#~\//}"
-  local internalPath= $(echo "$filepath" | sed 's|/\.\([^/]*\)|/dot_\1|g')
+  local internalPath= "$(echo "$filepath" | sed 's|/\.\([^/]*\)|/dot_\1|g')"
   bw unlock
   chezmoi add --template $1 
   echo "{\"organizationId\":null,\"folderId\":null,\"type\":2,\"name\":\"$2\",\"notes\":\"$(cat $1 | base64)\",\"favorite\":false,\"fields\":[],\"login\":null,\"secureNote\":{\"type\":0},\"card\":null,\"identity\":null}" | bw encode | bw create item

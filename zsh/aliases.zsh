e62e24360524a10191bd69da6d1377eb778d817b
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-04 13:48:39 -0400

    Update zsh/aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,17 +4,10 @@ alias vi='nvim'
 
 # Functions -------------------------------------------------------------------
 function addsecret () {
-  local filepath="${1/#~\//}"
-  local internalPath="$(echo $filepath | sed 's|/\.\([^/]*\)|/dot_\1|g')"
   bw unlock
-  chezmoi add --template $1 
-  echo "{\"organizationId\":null,\"folderId\":null,\"type\":2,\"name\":\"$2\",\"notes\":\"$(cat $1 | base64)\",\"favorite\":false,\"fields\":[],\"login\":null,\"secureNote\":{\"type\":0},\"card\":null,\"identity\":null}" | bw encode | bw create item
-  chezmoi cd
-  if  [ $# -eq 3 ]; then
-    echo "{{ (bitwarden "item" "$2").notes | b64dec }}" > $3
-  else
-    echo "{{ (bitwarden "item" "$2").notes | b64dec }}" > $internalPath.tmpl
-  fi
-  exit
+  echo "{\"organizationId\":null,\"folderId\":null,\"type\":2,\"name\":\"$2\",\"notes\":\"$(cat $1 | base64)\",\"favorite\":false,\"fields\":[],\"login\":null,\"secureNote\":{\"type\":0},\"card\":null,\"identity\":null}" | bw encode | bw create item 
   bw lock
+  chezmoi cd
+  echo "Echo the following into the template: {{ (bitwarden \"item\" \"$2\").notes | b64dec}"
+
 }

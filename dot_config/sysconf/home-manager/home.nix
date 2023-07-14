0ad2d693dc848c68eba95c16318f5a430c0434c7
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-14 19:18:18 -0400

    Update .config/sysconf/home-manager/home.nix Update .p10k.zsh

--- dot_config/sysconf/home-manager/home.nix
+++ dot_config/sysconf/home-manager/home.nix
@@ -128,6 +128,7 @@ in
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
+    EDITOR="nvim";
   };
 
   # Let Home Manager install and manage itself.

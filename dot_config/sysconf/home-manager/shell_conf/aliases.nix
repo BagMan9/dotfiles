20b2ce195f9ab18ed144f3f2891a2030a34bd9e0
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-14 19:33:47 -0400

    Update .config/sysconf/home-manager/shell_conf/aliases.nix

--- dot_config/sysconf/home-manager/shell_conf/aliases.nix
+++ dot_config/sysconf/home-manager/shell_conf/aliases.nix
@@ -4,6 +4,6 @@
     chezup="chezmoi add ~/.config/nvim/lazy-lock.json && chezmoi update";
     sysup="darwin-rebuild switch --flake ~/.config/sysconf/";
     hmedit="chezmoi edit ~/.config/sysconf/home-manager";
-    sysedit="chezmoi edit ~/.config/sysconf/";
+    sysedit="chezmoi edit ~/.config/sysconf";
     fullup="chezup && sysup && source ~/.zshrc";
 }

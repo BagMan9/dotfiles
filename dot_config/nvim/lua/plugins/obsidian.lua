b1754641e703cee1a6cf3a8838e326fd866ab382
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-07-18 21:29:29 -0400

    Add .config/nvim/lua/plugins/obsidian.lua

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    opts = {
+      dir = "/Users/isaacgrannis/Library/Mobile Documents/iCloud-md-obsidian/Documents/Main Vault",
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown" },
+      },
+    },
+  },
+}

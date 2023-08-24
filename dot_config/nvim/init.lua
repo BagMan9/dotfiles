73422aed1393ed98234fcd6a165c5005b480e350
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-08-24 10:59:03 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,4 +3,6 @@ require("config.lazy")
 
 -- LSP Init Stuff
 require("lspconfig").jedi_language_server.setup {}
-require("lspconfig").ccls.setup {}
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })

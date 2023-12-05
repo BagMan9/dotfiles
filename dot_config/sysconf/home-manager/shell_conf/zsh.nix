53990b2c1af99f8361d9f5ca9f32e0bb66e1a50e
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-05 18:15:22 -0500

    Update .config/sysconf/home-manager/shell_conf/zsh.nix

--- dot_config/sysconf/home-manager/shell_conf/zsh.nix
+++ dot_config/sysconf/home-manager/shell_conf/zsh.nix
@@ -38,7 +38,7 @@
           owner = "catppuccin";
           repo = "zsh-syntax-highlighting";
           rev = "06d519c20798f0ebe275fc3a8101841faaeee8ea";
-          hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          hash = "sha256-Q7KmwUd9fblprL55W0Sf4g7lRcemnhjh4/v+TacJSfo=";
         };
       }
   ];

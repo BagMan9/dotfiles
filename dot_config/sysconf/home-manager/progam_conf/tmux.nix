74d634148ce8e8f3641d4ed1dac4943c9e365023
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-04 23:14:44 -0500

    Update .config/sysconf/home-manager/progam_conf/tmux.nix

--- dot_config/sysconf/home-manager/progam_conf/tmux.nix
+++ dot_config/sysconf/home-manager/progam_conf/tmux.nix
@@ -10,17 +10,16 @@
       newSession = true;
       prefix = "C-Space";
       terminal = "xterm-256color";
-      plugins = with pkgs; [
-          tmuxPlugins.vim-tmux-navigator
-          tmuxPlugins.yank
-          gitmux
-          tmuxPlugins.tmux-fzf
-          tmuxPlugins.power-theme
-          tmuxPlugins.prefix-highlight
-          tmuxPlugins.battery
-          tmuxPlugins.resurrect
-          tmuxPlugins.tmux-thumbs
-          tmuxPlugins.fzf-tmux-url
+      plugins = with pkgs.tmuxPlugins; [
+          vim-tmux-navigator
+          yank
+          tmux-fzf
+          power-theme
+          prefix-highlight
+          battery
+          resurrect
+          tmux-thumbs
+          fzf-tmux-url
       ];
       extraConfig = ''
         bind v split-window -v -c "#{pane_current_path}"

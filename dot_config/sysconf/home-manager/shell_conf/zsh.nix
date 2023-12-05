abc0833b3ce7be28e5b1c994be2eb0b851181600
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-05 17:13:07 -0500

    Update .config/sysconf/home-manager/shell_conf/zsh.nix

--- dot_config/sysconf/home-manager/shell_conf/zsh.nix
+++ dot_config/sysconf/home-manager/shell_conf/zsh.nix
@@ -37,7 +37,7 @@
           owner = "zdharma-continuum";
           repo = "fast-syntax-highlighting";
           rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
-          hash = "sha256-1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g35ijzsma5";
+          hash = "sha256-1bmrb724vphw7y2gwn63rfssz3i8lp75ndjvlk5ns1g=";
         };
       }
   ];

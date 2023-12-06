c4a736cd7e556f0be0c958d0f513906073f82b8d
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-06 11:29:21 -0500

    Update .config/sysconf/home-manager/shell_conf/zsh.nix

--- dot_config/sysconf/home-manager/shell_conf/zsh.nix
+++ dot_config/sysconf/home-manager/shell_conf/zsh.nix
@@ -13,6 +13,11 @@
 
   initExtra = ''
       [[ ! -f ~/.config/p10k/p10k.zsh ]] || source ~/.config/p10k/p10k.zsh
+      tmux-window-name() {
+	        ($TMUX_PLUGIN_MANAGER_PATH/tmux-window-name/scripts/rename_session_windows.py &)
+      }
+
+      add-zsh-hook chpwd tmux-window-name
   '' + (import ./functions.nix {a=0;});
 
   profileExtra = ''

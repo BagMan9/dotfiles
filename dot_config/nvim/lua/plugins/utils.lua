26f6064f2f7a9c1c624b75f2469ca8049b86d205
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-19 16:52:27 -0500

    Update .config/nvim/lua/plugins/utils.lua

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- Git Wrapper [":G add "]
     "tpope/vim-fugitive",
   },
   {
@@ -9,10 +10,11 @@ return {
     end,
   },
   {
+    -- Github Issues
     "pwntester/octo.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope.nvim',
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
     },
     config = function()
       require("octo").setup()

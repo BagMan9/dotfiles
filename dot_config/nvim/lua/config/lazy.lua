467cf9e0a24b27a3b9fe339b2c4edafdd83f3fd7
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2023-12-18 20:15:02 -0500

    Update .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/plugins/line.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {

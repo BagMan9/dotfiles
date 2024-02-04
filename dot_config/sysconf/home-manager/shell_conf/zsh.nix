c11c605eb0ffed9d3ab36cbc0dbd1dfd881cb60b
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2024-02-03 22:31:51 -0500

    Update .config/sysconf/home-manager/shell_conf/zsh.nix

--- dot_config/sysconf/home-manager/shell_conf/zsh.nix
+++ dot_config/sysconf/home-manager/shell_conf/zsh.nix
@@ -23,15 +23,7 @@
       path+=('/Users/isaacgrannis/.local/bin/')
       "
 
-      # Shell-GPT integration ZSH v0.2
-      function _sgpt_zsh() {
-      if [[ -n "$BUFFER" ]]; then
-            _sgpt_prev_cmd=$BUFFER
-            BUFFER+="⌛"
-            zle -I && zle redisplay
-            BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd" --no-interaction)
-            zle end-of-line
-      fi
+     fi
         }
       zle -N _sgpt_zsh
       bindkey ^l _sgpt_zsh

f4a1841102e42fc27fcc98b3309da4f6a2b5d241
Isaac Grannis <115715725+BagMan9@users.noreply.github.com>
2024-02-06 16:06:35 -0500

    Update .config/nvim/lua/plugins/lsp-conf.lua

--- dot_config/nvim/lua/plugins/lsp-conf.lua
+++ dot_config/nvim/lua/plugins/lsp-conf.lua
@@ -6,6 +6,13 @@ return {
       servers = {
         clangd = {
           cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
             "--enable-config",
           },
         },
